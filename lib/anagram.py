49434b682ce9e2c1c55bf4ccee0321de9dcc2abf
Alvin Muira <alvinmuira@gmail.com>
2025-09-25 14:51:20 +0300

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_words):
+        sorted_word = sorted(self.word)
+        return [word for word in list_of_words if sorted(word) == sorted_word]
